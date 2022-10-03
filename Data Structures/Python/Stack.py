0300e819dade3e38b5cbb68ba84d8066907653d0
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-10-03 11:19:16 +0300

    deleted another varient of stack use

--- Data Structures/Python/Stack.py
+++ Data Structures/Python/Stack.py
@@ -1,32 +1,3 @@
-# a = input("Введите строку: ")
-# stack = []
-# flVerify = True
-#
-# for lt in a:
-#     if lt in "([{":
-#         stack.append(lt)
-#     elif lt in ")]}":
-#         if len(stack) == 0:
-#             flVerify = False
-#             break
-#
-#         br = stack.pop()
-#         if br == '(' and lt == ')':
-#             continue
-#         elif br == '[' and lt == ']':
-#             continue
-#         elif br == '{' and lt == '}':
-#             continue
-#
-#         flVerify = False
-#         break
-#
-# if flVerify and len(stack) == 0:  # stack must be empty
-#     print("Yes")
-# else:
-#     print("No")
-
-
 class Stack:
     def __init__(self):
         self.collection = []
